d64af9c01d7f27d48254a02b3c67ec0c93643577
michaelxiao16 <michaelxiao16@gmail.com>
2018-03-02 23:13:43 -0500

    Added fake data to populate bar graph

--- app/src/main/java/xmts/gaintrain/Fragments/Statistics.java
+++ app/src/main/java/xmts/gaintrain/Fragments/Statistics.java
@@ -2,6 +2,7 @@ package xmts.gaintrain.Fragments;
 
 /**
  * Created by xu on 3/2/18.
+ *
  */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,20 +11,43 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import xmts.gaintrain.R;
 
 public class Statistics extends Fragment {
+
+
     public Statistics() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.statistics_fragment, container, false);
-
         BarChart chart = (BarChart) view.findViewById(R.id.weeklybargraph);
+
+        List<BarEntry> fakedata = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            fakedata.add(new BarEntry(i, i));
+        }
+        BarDataSet dataSet = new BarDataSet(fakedata, "Day");
+        BarData data = new BarData(dataSet);
+        chart.setData(data);
+        Description bullshitDescription = new Description();
+        bullshitDescription.setText("BULLSHIT LEVEL OVER TIME CHART");
+        chart.setDescription(bullshitDescription);
+
         return view;
     }
+
     @Override
     public void onPause() {
         super.onPause();
