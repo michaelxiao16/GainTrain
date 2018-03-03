0f1fb7e9d2417f1f039d532d935454272ec99eb9
michaelxiao16 <michaelxiao16@gmail.com>
2018-03-02 20:38:56 -0500

    Added fields to passenger class for statistics

--- app/src/main/java/xmts/gaintrain/Models/Passenger.java
+++ app/src/main/java/xmts/gaintrain/Models/Passenger.java
@@ -6,4 +6,33 @@ package xmts.gaintrain.Models;
  */
 
 public class Passenger {
+    public int daysOnTrain;
+    public int workoutsThisWeek;
+    public int avgWorkoutLength;
+    public int totalVolumne;
+
+    public Passenger(){
+        //Default constructor required;
+        this(0,0,0,0);
+    }
+
+    public Passenger(int days, int workouts, int length, int volume) {
+        this.daysOnTrain =  days;
+        this.workoutsThisWeek = workouts;
+        this.avgWorkoutLength = length;
+        this.totalVolumne = volume;
+    }
+
+    public int getDaysOnTrain() {
+        return daysOnTrain;
+    }
+    public int getWorkoutsThisWeek() {
+        return workoutsThisWeek;
+    }
+    public int getAvgWorkoutLength() {
+        return avgWorkoutLength;
+    }
+    public int getTotalVolumne() {
+        return totalVolumne;
+    }
 }
